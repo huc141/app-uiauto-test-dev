eb427fa7f7df610a8404dc2b9cdbfc5190452926
huc141 <1349983371@qq.com>
2024-05-16 00:01:18 +0800

    优化logger.py代码

--- common_tools/logger.py
+++ common_tools/logger.py
@@ -5,10 +5,13 @@ import os
 
 
 class Logger:
-    def __init__(self, name: str = 'data', log_dir: str = None, level: str = 'logging.INFO') -> None:
+    def __init__(self, name: str = 'data', level=logging.INFO) -> None:
         self._name = name
-        self._log_dir = log_dir
+        os.chdir("..")  # 注意：修改pycharm运行配置后，注销该行代码。
+        self._log_dir = os.path.join(os.getcwd(), 'log')
+        print(self._log_dir)
         self._level = level
+        self.here = os.path.abspath(os.path.dirname(__file__))
 
     # def get_time_by_fmt(self, fmt: str = '%Y-%m-%d', dt: str = time.localtime()) -> str:
     #     return time.strftime(fmt, dt)
@@ -16,23 +19,30 @@ class Logger:
     # p = os.path.join(get_time_by_fmt('%Y%m%d'), 'runtime.log')
     # print(p)
 
-    def setup_logging(self, log_file='./log.txt', log_level=''):
+    def setup_logging(self):
         # 创建日志记录器
         logger = logging.getLogger()
-        logger.setLevel(log_level)
+        logger.setLevel(self._level)
 
         # 设置日志输出格式
-        format_log = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+        # format_log = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+        format_log = logging.Formatter('%(name)s  %(asctime)s %(filename)s[line:%(lineno)d]  %(levelname)s  [%(module)s - %(funcName)s]: %(message)s')
 
         # 创建一个Handler用于将日志写入文件
-        fh = logging.FileHandler(log_file, mode='a', encoding='utf-8')
-        fh.setLevel(log_level)
+        # fh = logging.FileHandler(log_file, mode='a', encoding='utf-8')
+        # when 参数设置为 'midnight'，表示每天的午夜会生成一个新的日志文件。
+        # interval 参数设置为 1，表示每天生成一个新的日志文件。
+        # backupCount 参数设置为 7，表示保留最近的 7 个日志文件备份
+        log_file = os.path.join(self._log_dir, 'running_log')
+        fh = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=7)
+        fh.setLevel(self._level)
         fh.setFormatter(format_log)
+        fh.suffix = "%Y-%m-%d.log"  # 设置日志文件名后缀格式
         logger.addHandler(fh)
 
         # 创建一个Handler用于控制台输出日志
         ch = logging.StreamHandler()
-        ch.setLevel(log_level)
+        ch.setLevel(self._level)
         ch.setFormatter(format_log)
         logger.addHandler(ch)
 
