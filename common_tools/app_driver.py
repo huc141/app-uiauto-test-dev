dcf58ec82e8ea794adf5d7bff8b0be0715813d00
luoyulong <luoyl@reolink.com.cn>
2024-05-16 16:50:56 +0800

    优化app_driver代码，新增异常捕获

--- common_tools/app_driver.py
+++ common_tools/app_driver.py
@@ -1,5 +1,7 @@
+import logging
 import uiautomator2 as u2
 from common_tools.read_yaml import read_yaml
+from common_tools.logger import logger
 
 
 class Driver:
@@ -11,49 +13,76 @@ class Driver:
 
     # 连接手机
     def init_driver(self):
-        self._driver = u2.connect_usb(self._device_sn)
-        return self._driver
+        logger.info("开始USB连接手机")
+        try:
+            self._driver = u2.connect_usb(self._device_sn)
+            logger.info("连接成功")
+            return self._driver
+        except Exception as err:
+            logger.error("连接失败，原因为：{}".format(err))
+            return None
 
     # 启动reolink app
     def start(self):
-        if self._driver:
-            self._driver.app_start(self._apk_name)
-            all_paks = self._driver.app_list()  # 列出所有正在运行的APP，返回一个列表
-            running_app = self._driver.app_current()  # 获取当前打开的APP信息，返回一个字典
-            print("当前手机：%s 正在运行的所有app包名：%s" % (self._device_sn, all_paks))
-            print("当前手机：%s 正在运行的app包名： %s" % (self._device_sn, running_app))
-            return all_paks, running_app
+        try:
+            if self._driver:
+                logger.info("开始启动app···")
+                self._driver.app_start(self._apk_name)
+                all_paks = self._driver.app_list()  # 列出所有正在运行的APP，返回一个列表
+                running_app = self._driver.app_current()  # 获取当前打开的APP信息，返回一个字典
+                logger.info("app启动成功···")
+                logger.info("当前手机：%s 正在运行的app包名： %s" % (self._device_sn, running_app))
+                return all_paks, running_app
+        except Exception as err:
+            logger.error("APP启动失败，原因为：%s", err)
 
     # 获取reolink app的信息
     def get_app_info(self) -> dict:
-        if self._driver:
-            app_info = self._driver.app_info(self._apk_name)
-            # print(app_info)
-            return app_info
+        try:
+            if self._driver:
+                app_info = self._driver.app_info(self._apk_name)
+                logger.info("app信息获取成功···")
+                return app_info
+        except Exception as err:
+            logger.error("APP信息获取失败，原因为：%s", err)
 
     # 获取手机信息
     def get_device_info(self) -> dict:
-        if self._driver:
-            device_info = self._driver.device_info
-            return device_info
+        try:
+            if self._driver:
+                device_info = self._driver.device_info
+                logger.info("获取手机信息成功···")
+                return device_info
+        except Exception as err:
+            logger.error("获取手机信息失败，原因为：%s", err)
 
     # 安装app
     def install_app(self):
-        if self._driver:
-            self._driver.app_install(self._apk_local_path)
+        try:
+            if self._driver:
+                self._driver.app_install(self._apk_local_path)
+                logger.info("app安装成功···")
+        except Exception as err:
+            logger.error("app安装失败，原因为：%s", err)
 
     # 卸载reolink app
     def uninstall_app(self):
-        if self._apk_name in self._driver.app_list():
-            self._driver.app_uninstall(self._apk_name)
+        try:
+            if self._apk_name in self._driver.app_list():
+                self._driver.app_uninstall(self._apk_name)
+                logger.info("卸载reolink app成功")
+        except Exception as err:
+            logger.error("卸载app失败，原因：%s", err)
 
     # 清除app缓存
     def clear_app_cache(self):
         if self._driver:
             self._driver.app_clear(self._apk_name)
+            logger.info("清除reolink app缓存成功···")
 
     def stop(self):
         self._driver.app_stop(self._apk_name)
+        logger.info("reolink app已停止运行")
 
 
 driver = Driver(device_sn=read_yaml.config_device_sn, apk_name=read_yaml.config_apk_name)
