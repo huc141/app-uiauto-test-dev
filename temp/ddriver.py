147498449dd363e724c44c36b15efc4ddf05564d
huc141 <1349983371@qq.com>
2024-05-28 00:34:01 +0800

    新增appium测试代码

--- temp/ddriver.py
+++ temp/ddriver.py
@@ -1,39 +1,16 @@
-import subprocess
-import uiautomator2 as u2
-from appium.options.android import UiAutomator2Options
-from common_tools.read_yaml import read_yaml
-from common_tools.logger import logger
-import os
 from appium import webdriver
+from appium.options.android import UiAutomator2Options
 
-appium_server_url = 'http://localhost:4723/'
-
-
-class DDriver:
-    def __init__(self, device_sn: str, apk_name: str = '', apk_local_path: str = read_yaml.config_apk_local_path):
-        self._device_sn = device_sn
-        self._apk_name = apk_name
-        self._apk_local_path = apk_local_path
-        self._driver = None
-
-    def get_android_driver(self, is_native):
-        if is_native:
-            app_package = self._apk_name
-            app_activity = 'com.android.bc.MainActivity'
-        else:
-            app_package = 'com.rnclient'
-            app_activity = 'com.android.bc.home.HomeActivity'
-
-        capabilities = dict(
-            platformName='Android',
-            deviceName='Android',
-            automationName='uiautomator2',
-            appPackage=app_package,
-            appActivity=app_activity,
-            noReset=True
-        )
-        driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
-        return driver
+appium_server_url = 'http://localhost:4723'
+capabilities = {
+    'platformName': 'Android',
+    'automationName': 'uiautomator2',
+    'deviceName': '127.0.0.1:62001',
+    'appPackage': 'com.mcu.reolink',
+    'appActivity': 'com.android.bc.MainActivity',
+}
+capabilities_options = UiAutomator2Options().load_capabilities(capabilities)
 
 
-driver = DDriver(device_sn=read_yaml.config_device_sn, apk_name=read_yaml.config_apk_name)
+def test_driver():
+    webdriver.Remote(command_executor=appium_server_url, options=capabilities_options)
--- temp/test.py
+++ temp/test.py
@@ -1,25 +1,167 @@
-# 获取预期结果 test_data["expected"]
-# 断言
-# 如果出现断言失败，需要将失败的用例记录到logger当中
-# 如果断言失败，会抛出一个异常，AssertionError
-# 如果不手动抛出异常，程序正常走不是走try就是except分支，测试用例都会全部显示通过；
-# 抛出异常就意味着程序运行错误，这条用例执行失败
-try:
-    print(res["msg"])
-    self.assertEqual(test_data["expected"], res["code"])
-    # 把实际结果写入excel数据，通过case_id获取行号
-    self.excel_handler.write(config.data_path,
-                             "register",
-                             test_data["case_id"] + 1,
-                             9,
-                             "测试通过")
-except AssertionError as e:  # 如果出现错误，就会执行except的代码
-    # 记录日志logger
-    self.logger.error("测试用例失败：{}".format(e))
-    # 把实际结果写入excel数据，通过case_id获取行号
-    self.excel_handler.write(config.data_path,
-                             "register",
-                             test_data["case_id"] + 1,
-                             9,
-                             "测试失败")
-    raise e  # 程序运行错误，抛出异常，就意味着这条用例执行失败
+# @Fuction  :主要是页面的一些操作
+
+import allure
+from datetime import datetime
+from appium import webdriver
+from commons.deal_log import Logs
+from configs.config import ImagePath
+
+class Operates():
+    # 初始化页面的操作
+    def __init__(self):
+        """
+        构造函数，创建必要的实例变量
+        """
+        self.log = Logs().get_log()  # 初始化一个log对象
+
+    # def init_driver(self):
+    #     """
+    #     告诉appium自动化测试相关的配置项
+    #     :return: 返回驱动
+    #     """
+
+        caps = {
+            # 被测APP所处平台-操作系统
+            'platformName': 'Android',
+            # 操作系统版本
+            'platformVersion': '10',
+            # 设备明后才能——可以随表填写，但是必须要有
+            'deviceName': 'reolink',
+            # 被测APP的信息————打开某个APP后输入命令：adb shell dumpsys activity recents | findstr intent
+            # cmd上展示的第一行命令：com.android.mediacenter/.PageActivity
+            # 包名——代表被测app在设备上的地址
+            'appPackage': 'com.mcu.reolink',
+            # 入口信息——被测app入口
+            'appActivity': 'com.android.bc.MainActivity',
+            # 禁止app在自动化后重置
+            'noReset': True,
+            # 设置命令超时时间,超过后driver会关闭
+            'newCommandTimeout': 3600,
+            # 指定驱动——UI2,安卓5以下用uiautomator1，以上用uiautomator2
+            'automationName': 'UiAutomator2',
+            # 支持中文
+            'unicodeKeyboard': True,  # 使用 Unicode 输入法
+            'resetKeyboard': True,  # 在设定了 `unicodeKeyboard` 关键字的 Unicode 测试结束后，重置输入法到原有状态
+        }
+
+        # 启动被测试app,启动之前打开appium server
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', caps)  # 如果访问的是本机就用localhost或127.0.0.1, wd/hub是固定的
+        self.driver.implicitly_wait(10)  # 设置隐士等待10s
+        # return driver
+
+    def click(self, *locator):
+        """
+        找到元素，并点击
+        :param locator: 定位器
+        :return:
+        """
+        try:
+            self.driver.find_element(*locator).click()
+        except Exception:
+            self.log.error("定位点击元素失败！")
+
+    def click_ele_exist(self, *locator):
+        """
+        当定为元素出现时，定位元素，并点击
+        :param locator: 定位器
+        :return:
+        """
+        try:
+            self.driver.find_element(*locator)
+            btns = self.driver.find_elements(*locator)
+            # 如果出现用户协议弹出按钮
+            if btns:
+                btns[0].click()
+        except Exception:
+            self.log.error("定位有时出现的点击元素失败！")
+
+    def input_text(self, value, *locator):
+        """
+        定位元素，并完成输入
+        :param text:
+        :param locator:
+        :return:
+        """
+        try:
+            self.driver.find_element(*locator).send_keys(str(value))
+        except Exception:
+            self.log.info("定位输入元素失败！")
+
+
+    def get_text(self, *locator):
+        """
+        获取文本元素
+        :param locator:
+        :return:
+        """
+        try:
+            ele = self.driver.find_element(*locator)
+        except Exception:
+            self.log.error("获取元素文本失败！")
+        else:
+            return ele.text
+
+    def getImage(self, image_name):
+        """
+        生成用例失败的截图,并将截图展示到allure报告中
+        :param image_name: 截图的名称
+        :return:
+        """
+        try:
+            nowTime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            NewPicture = ImagePath + '\\' + nowTime + '_' + image_name + '.png'   # 保存图片为png格式
+            self.driver.get_screenshot_as_file(NewPicture)
+            allure.attach.file(NewPicture, attachment_type=allure.attachment_type.PNG)  # 将截图作为附件上传到allure测试报告中
+        except Exception:
+            self.log.error(u'截图失败！')
+
+    def getElements(self, *locator):
+        """
+        根据某种方式定位到多个元素
+        :return:
+        """
+        try:
+            eles = self.driver.find_elements(*locator)
+        except Exception:
+            self.log.error("定位多个元素失败！")
+        else:
+            return eles
+
+    def new_swap(self, start_x, start_y, end_x, end_y):
+        """
+        重新疯转swap()函数
+        滑动页面
+        :param start_x: 当前位置的横坐标
+        :param start_y: 当前位置的竖坐标
+        :param end_x: 滑动后位置的横坐标
+        :param end_y: 滑动后位置的竖坐标
+        :return:
+        """
+        try:
+            self.driver.swap(start_x, start_y, end_x, end_y)
+        except Exception:
+            self.log.error("滑动页面失败！")
+
+    def new_keyEvent(self, key):
+        """
+        重新封装keyEvent()函数
+        根据不同的键值，来确定系统的操作
+        :param key:
+        :return:
+        """
+        try:
+            self.driver.keyevent(key)
+        except Exception:
+            self.log.error("系统键操作失败！")
+
+    def quit(self):
+        """
+        退出浏览器
+        :return:
+        """
+        try:
+            self.driver.quit()
+        except Exception:
+            self.log.error("退出浏览器失败！")
+
+
