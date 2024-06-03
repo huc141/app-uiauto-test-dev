7601864c3dffc5e52185cb64d84b4ec2ed53c588
huc141 <1349983371@qq.com>
2024-06-04 00:46:34 +0800

    优化代码

--- pages/welcome_page.py
+++ pages/welcome_page.py
@@ -13,18 +13,18 @@ class WelcomePage(BasePage):
         点击勾选启动页自动弹出的【声明与条款】单选按钮
         :return:
         """
-        return self.click_by_id(id_name=self.agree_term_button)
+        return self.click_by_id(resource_id=self.agree_term_button)
 
     def click_agree_continue_btn(self):
         """
         点击欢迎页自动弹出的【声明与条款】的【同意并继续】按钮
         :return:
         """
-        return self.click_by_id(id_name=self.agree_continue_btn)
+        return self.click_by_id(resource_id=self.agree_continue_btn)
 
     def click_disagree_exit_btn(self):
         """
         点击欢迎页自动弹出的【声明与条款】的【不同意并退出】按钮
         :return:
         """
-        return self.click_by_id(id_name=self.disagree_exit_btn)
+        return self.click_by_id(resource_id=self.disagree_exit_btn)
