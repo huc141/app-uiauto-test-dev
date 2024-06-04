c003db5ff86347dd6be7972ecb01690d71122beb
huc141 <1349983371@qq.com>
2024-06-05 00:12:17 +0800

    新增权限弹窗处理逻辑

---
+++ common_tools/handle_alerts.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from common_tools.app_driver import driver
+
+DEFAULT_SECONDS = 15
+
+
+class HandleAlerts:
+    def __init__(self):
+        self.driver = driver.get_actual_driver()
+        self.driver.wait_timeout = DEFAULT_SECONDS  # 设置全局等待超时时间为15秒
+
+    def handle_alerts(self):
+        """
+        定义并处理常见弹窗
+        """
+        alert_buttons = [
+            'com.android.permissioncontroller:id/permission_allow_foreground_only_button'  # 系统权限弹窗：仅在使用该应用时允许
+        ]
+
+        for button in alert_buttons:
+            if self.driver(resourceId=button).exists:
+                print(f"识别到相关弹窗按钮: {button}")
+                self.driver(resourceId=button).click()
+                return True
+        return False
+
+
+handle_alert = HandleAlerts()
