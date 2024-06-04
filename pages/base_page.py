6212e37e2187d881d71588ed69432f3a57e8514d
huc141 <1349983371@qq.com>
2024-06-05 00:12:32 +0800

    优化代码

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ import time
 from common_tools.app_driver import driver
 from common_tools.logger import logger
 from typing import Literal
+from common_tools.handle_alerts import handle_alert
 
 DEFAULT_SECONDS = 15
 
@@ -14,40 +15,6 @@ class BasePage:
         #     self.driver = driver._driver
         self.driver = driver.get_actual_driver()
         self.driver.wait_timeout = DEFAULT_SECONDS  # 设置全局等待超时时间为15秒
-
-    def handle_alerts(self):
-        """
-        定义并处理常见弹窗
-        """
-        alert_buttons = [
-            'com.android.permissioncontroller:id/permission_allow_foreground_only_button'  # 系统权限弹窗：仅在使用该应用时允许
-        ]
-
-        for button in alert_buttons:
-            if self.driver(resourceId=button).exists:
-                print(f"Found alert with button: {button}")
-                self.driver(resourceId=button).click()
-                return True
-        return False
-
-    # def click_by_id(self, resource_id):
-    #     """通过id定位单个元素"""
-    #     try:
-    #         time.sleep(3)
-    #         element = self.driver(resourceId=resource_id)
-    #         if not element.exists():
-    #             logger.error("该点击元素：%s 不存在", resource_id)
-    #             raise ValueError(f"该点击元素：{resource_id} 不存在")
-    #
-    #         element.click()
-    #         time.sleep(3)
-    #     except ValueError as verr:  # 专门捕获并处理 ValueError 异常，可以在此处添加特定的处理逻辑。
-    #         logger.error("ValueError: %s", verr)
-    #         raise
-    #     except Exception as err:  # 捕获并处理所有其他类型的异常，确保程序不会因为未处理的异常而崩溃。
-    #         logger.error("页面中没有找到id为 %s 的元素，原因可能是：%s", resource_id, err)
-    #         raise
-    #     # return self.driver
 
     def click_by_id(self, resource_id, retries=2, is_click_again: Literal[True, False] = False):
         """
@@ -66,11 +33,11 @@ class BasePage:
 
                 element.click()
                 time.sleep(2)
-                if self.handle_alerts():
+                if handle_alert.handle_alerts():
                     if is_click_again:
-                        continue  # 如果处理了权限弹窗，重试点击操作
+                        continue  # 如果is_click_again==True，则处理了权限弹窗后，重试点击操作
                     else:
-                        return True
+                        return True  # 如果is_click_again==False，则处理了权限弹窗后，不重试点击。
                 return True
             except ValueError as verr:  # 专门捕获并处理 ValueError 异常，可以在此处添加特定的处理逻辑。
                 logger.error("ValueError: %s", verr)
@@ -98,11 +65,11 @@ class BasePage:
 
                 element.click()
                 time.sleep(2)
-                if self.handle_alerts():
+                if handle_alert.handle_alerts():
                     if is_click_again:
-                        continue  # 如果处理了权限弹窗，重试点击操作
+                        continue  # 如果is_click_again==True，则处理了权限弹窗后，重试点击操作
                     else:
-                        return True
+                        return True  # 如果is_click_again==False，则处理了权限弹窗后，不重试点击。
                 return True
             except ValueError as verr:  # 专门捕获并处理 ValueError 异常，可以在此处添加特定的处理逻辑。
                 logger.error("ValueError: %s", verr)
