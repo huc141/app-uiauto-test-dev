669b9f301ea698194d9e38e8328a635ac03e21fd
huc141 <1349983371@qq.com>
2024-06-09 01:25:17 +0800

    优化代码

--- test_case/test_add_device.py
+++ test_case/test_add_device.py
@@ -10,7 +10,7 @@ class TestAddDevice:
         device_list_page.click_add_device_button()  # 点击设备列表右上角的添加按钮
         add_device_page = AddDevicePage()  # 初始化添加设备页面的对象
         add_device_page.click_manual_input()  # 点击“手动输入”按钮
-        add_device_page.input_by_uid('952700Y002WY1J87')  # 输入uid并点击下一步
-        add_device_page.get_toast("这个设备已经存在")
+        add_device_page.input_by_uid('952700Y005S613CC')  # 输入uid并点击下一步
+        add_device_page.get_toast("11111")
         driver.stop()
 
