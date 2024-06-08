1fb984fb4aafacdb62a0cdca47db4dce02ddb206
huc141 <1349983371@qq.com>
2024-06-09 01:24:44 +0800

    优化代码

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,10 @@
+import logging
+import os
 import time
+from datetime import datetime
+
+import uiautomator2 as u2
+from uiautomator2.exceptions import XPathElementNotFoundError
 from common_tools.app_driver import driver
 from common_tools.logger import logger
 from typing import Literal
@@ -14,7 +20,15 @@ class BasePage:
         # else:
         #     self.driver = driver._driver
         self.driver = driver.get_actual_driver()
-        self.driver.wait_timeout = DEFAULT_SECONDS  # 设置全局等待超时时间为15秒
+        # self.driver.wait_timeout = DEFAULT_SECONDS
+
+    def find_element_xpath(self, xpath_expression):
+        """
+        通过xpath找元素
+        :param xpath_expression: xpath表达式
+        :return: 元素对象
+        """
+        return self.driver.xpath(xpath_expression)
 
     def click_by_id(self, resource_id, retries=2, is_click_again: Literal[True, False] = False):
         """
@@ -57,7 +71,7 @@ class BasePage:
         for attempt in range(retries):
             try:
                 time.sleep(2)
-                element = self.driver.xpath(xpath_expression)
+                element = self.find_element_xpath(xpath_expression)
                 if not element.exists:
                     logger.error("该点击元素：%s 不存在", xpath_expression)
                     raise ValueError(f"该点击元素：{xpath_expression} 不存在")
@@ -78,44 +92,78 @@ class BasePage:
                 raise
         return False
 
-    def get_toast(self, toast_text: str, reset=True, default="没有获取到toast提示"):
+    def get_toast(self, toast_text: str, reset=True, timeout=5.0, default="没有获取到toast提示"):
         """
-        获取页面弹出的toast
+        废除！该方法不可用！ 获取页面弹出的toast
+        :param timeout: 超时时间，默认5秒
         :param toast_text: 需要检查的toast文案
-        :param reset: 默认为True，即清除缓存的Toast，传False则不清除。
-        :param default: 如果10s内没有获取到Toast，则返回此默认消息。
+        :param reset: 默认为True，即清除缓存的Toast，传False则不清除
+        :param default: 如果10s内没有获取到Toast，则返回此默认消息
         :return:
         """
-        try:
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            try:
+                # 尝试获取Toast文本内容
+                toast_message = self.find_element_xpath('//*[contains(@text,{})]'.format(toast_text))
+                # 验证Toast消息内容是否符合预期
+                if toast_message:
+                    logger.info(f"Toast message found: {toast_message}")
+                    return toast_message
+            except XPathElementNotFoundError:
+                pass
 
-            # 尝试获取Toast消息
-            toast_message = self.driver.toast.get_message(wait_timeout=10, cache_timeout=10)
-            # 检查是否获取到Toast消息，如果没有则使用默认值
-            actual_message = toast_message if toast_message else default
-            # 验证Toast消息内容是否符合预期
-            if toast_text not in actual_message:
-                raise ValueError(f"预期的Toast文本：'{toast_text}' 未在实际Toast：'{actual_message}' 中找到")
-
-            # 根据配置决定是否重置Toast缓存
+            # 根据配置决定是否重置Toast缓存, 默认重置
             if reset:
                 self.driver.toast.reset()
 
-            return actual_message  # 成功匹配到Toast，返回其内容
+            # 每隔0.5秒重新尝试获取一次toast消息
+            time.sleep(0.5)
 
-        except Exception as err:
-            # 确保在异常情况下也能访问到实际获取的Toast消息或默认值
-            toast_message = self.driver.toast.get_message(wait_timeout=10, cache_timeout=10, default=default)
-            raise ValueError(f"获取Toast失败，实际消息：'{toast_message}'，原因：{err}")
+        # 确保在异常情况下也能访问到实际获取的Toast消息或默认值
+        logger.error(f"获取Toast失败，返回默认消息：'{default}'")
+        return default
+
+    def take_screenshot(self, device_type="Android"):
+        """
+        屏幕截图
+        :param device_type: 设备类型，根据设备类型保存截图位置
+        :return:
+        """
+        screenshot_save_path = os.path.join(os.getcwd(), 'screenshot/android')
+        # 确保保存目录存在
+        if not os.path.exists(screenshot_save_path):
+            os.makedirs(screenshot_save_path)
+
+        # 获取当前时间并格式化为字符串
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        if device_type.lower() == 'android':
+            # 使用uiautomator2截图逻辑
+            filename = f"{timestamp}-android.png"
+            filepath = os.path.join(screenshot_save_path, filename)
+            self.driver.screenshot(filepath)
+            logger.info("Taking screenshot on Android device.")
+        elif device_type.lower() == 'ios':
+            # TODO: 处理iOS设备的截图
+            print("Taking screenshot on iOS device.")
+        else:
+            raise ValueError("Unsupported device type. Please specify 'android' or 'ios'.")
 
     def input_text(self, text):
         """
-        输入文本,不清空文本框内容，直接输入。调用该方法时不要开启atx悬浮窗。
+        使用abd命令输入文本,不清空文本框内容，直接输入，不支持中文。
         :element: 编辑框的xpath表达式
         :text： 要输入的文本内容
         :return:
         """
         try:
+            self.driver.set_input_ime(True)
+            time.sleep(0.2)
             self.driver.send_keys(text)
+            # for char in text:
+            #     os.system('adb shell input text {}'.format(text))
+            #     time.sleep(0.2)
         except Exception as err:
             logger.error(f"文本输入失败：{err}")
 
