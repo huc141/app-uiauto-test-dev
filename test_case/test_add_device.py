12fc8bb85cc3a7e2fb0fe8b677589398f00b13eb
luoyulong <luoyl@reolink.com.cn>
2024-07-06 18:16:29 +0800

    调试参数化(未完成)

--- test_case/test_add_device.py
+++ test_case/test_add_device.py
@@ -5,13 +5,13 @@ from common_tools.app_driver import driver
 
 
 class TestAddDevice:
-    # @pytest.mark.parametrize()
+    # @pytest.mark.parametrize("uid, identifier", uid, identifier)
     def test_add_device_by_uid(self):
         driver.start(True)
         device_list_page = DeviceListPage()  # 初始化设备列表对像
         device_list_page.click_add_device_button()  # 点击设备列表右上角的添加按钮
         add_device_page = AddDevicePage()  # 初始化添加设备页面的对象
-        add_device_page.click_manual_input("uid", "952700Y005S613CC")  # 点击“添加”设备按钮并点击“手动输入”按钮
+        add_device_page.click_manual_input("uid", "952700Y006TG1NQO")  # 点击“添加”设备按钮并点击“手动输入”按钮
         # add_device_page.input_by_uid('')  # 输入uid并点击下一步
 
     def test_add_device_by_ip(self):
