8c9b0eff1d46f21308d09ff287138a3c81816479
luoyulong <luoyl@reolink.com.cn>
2024-07-12 18:41:25 +0800

    新增检查预期文本的方法

--- pages/rn_device_setting_page/remote_setting.py
+++ pages/rn_device_setting_page/remote_setting.py
@@ -12,6 +12,18 @@ class RemoteSetting(BasePage):
         elif self.platform == 'ios':
             pass
 
+    def check_remote_setting_text(self, text_to_check):
+        """
+        检查文本
+        :param text_to_check: 需要检查的预期文本
+        :return:
+        """
+        self.scroll_and_click_by_text(el_type="text", text_to_find=text_to_check)
 
-def check_remote_setting_text(self):
-    pass
+    def scroll_click_remote_setting(self, device_name):
+        """
+        逐一滚动查找设备名称并点击远程设置按钮
+        :param device_name: 要查找的设备名称
+        :return:
+        """
+        self.access_in_remote_setting(text_to_find=device_name)
