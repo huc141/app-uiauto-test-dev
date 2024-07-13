eb1eece3f1ffd17975446087aabac597588a6606
huc141 <1349983371@qq.com>
2024-07-14 00:49:20 +0800

    调试

--- debug/test_my_temp.py
+++ debug/test_my_temp.py
@@ -1,9 +1,1 @@
-from common_tools.app_driver import driver
-from pages.device_list_page import DeviceListPage
 
-
-class TestMyTemp:
-    def test_add_device_by_uid(self):
-        driver.start_app()
-        device_list_page = DeviceListPage()  # 初始化设备列表对像
-        device_list_page.take_screenshot()
