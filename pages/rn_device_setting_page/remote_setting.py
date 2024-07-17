c9814861db51f7e4ded641e8d60deb08eeec9932
huc141 <1349983371@qq.com>
2024-07-17 13:00:03 +0800

    更新远程配置主页公共方法

--- pages/rn_device_setting_page/remote_setting.py
+++ pages/rn_device_setting_page/remote_setting.py
@@ -12,14 +12,21 @@ class RemoteSetting(BasePage):
         elif self.platform == 'ios':
             pass
 
-    def check_remote_setting_text(self, expected_text, exclude_texts):
+    def check_remote_setting_text(self, expected_text, exclude_texts,
+                                  xml_az_parse_conditions, xml_ios_parse_conditions):
         """
         根据设备名，检查对应设备的远程配置功能是否和预期一致
+        :param xml_az_parse_conditions: 安卓的远程配置主(一级)页面解析条件，用于排除无关文本，筛选出页面功能
+        :param xml_ios_parse_conditions: iOS的远程配置主(一级)页面解析条件，用于排除无关文本，筛选出页面功能
         :param expected_text: 需要检查的预期文本
         :param exclude_texts: 需要排除的文本
         :return:
         """
-        self.verify_page_text(expected_text=expected_text, exclude_texts=exclude_texts)
+        return self.verify_page_text(expected_text=expected_text,
+                                     exclude_texts=exclude_texts,
+                                     xml_az_parse_conditions=xml_az_parse_conditions,
+                                     xml_ios_parse_conditions=xml_ios_parse_conditions
+                                     )
 
     def scroll_click_remote_setting(self, device_name):
         """
@@ -27,4 +34,4 @@ class RemoteSetting(BasePage):
         :param device_name: 要查找的设备名称
         :return:
         """
-        self.access_in_remote_setting(text_to_find=device_name)
+        return self.access_in_remote_setting(text_to_find=device_name)
