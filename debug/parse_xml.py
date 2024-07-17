572abe1e9480b8f4ea1c7961ac2a81088aa5ffc9
huc141 <1349983371@qq.com>
2024-07-18 01:13:13 +0800

    调试

--- debug/parse_xml.py
+++ debug/parse_xml.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 
 
-def parse_and_extract_text(xml_path, output_path, package, class_name, index, exclude_texts=None):
+def parse_and_extract_text(xml_path, output_path, exclude_texts=None):
     if exclude_texts is None:
         exclude_texts = []
 
@@ -11,14 +11,13 @@ def parse_and_extract_text(xml_path, output_path, package, class_name, index, ex
     texts = set()
 
     for elem in root.iter():
-        if (elem.attrib.get('package') == package and
-                elem.attrib.get('class') == class_name and
-                elem.attrib.get('index') == index and
-                elem.attrib.get('text') != "" and
-                elem.attrib.get('resource-id') != 'com.mcu.reolink:id/device_name_tv' and
-                elem.attrib.get('enabled') == "true"):
+        if (elem.attrib.get('type') == "XCUIElementTypeStaticText" and
+                elem.attrib.get('index') == "0" and
+                elem.attrib.get('label') != "" and
+                elem.attrib.get('x') != "0" and
+                elem.attrib.get('accessible') != 'false'):
 
-            text = elem.attrib.get('text')
+            text = elem.attrib.get('label')
 
             if text and text not in exclude_texts:
                 texts.add(text)
@@ -33,11 +32,8 @@ def parse_and_extract_text(xml_path, output_path, package, class_name, index, ex
     print(f"总非空行数: {non_empty_lines}")
 
 
-xml_path = 'D:\\app-uiauto-test-dev\\debug\\destination4.xml'
-output_path = 'D:\\app-uiauto-test-dev\\elements_texts.txt'
-package = "com.mcu.reolink"
-class_name = "android.widget.TextView"
-index = "0"
-exclude_texts = ["开", "关", "设置", "报警设置", "报警通知", "更多", "0", "退出登录设备", "删除"]
+xml_path = 'H:\\app-uiauto-test-dev\\debug\\destination4.xml'
+output_path = 'H:\\app-uiauto-test-dev\\elements_texts.txt'
+exclude_texts = ["开", "关", "设置", "报警设置", "报警通知", "更多", "0", "退出登录设备", "删除", "FE-W2"]
 
-parse_and_extract_text(xml_path, output_path, package, class_name, index, exclude_texts)
+parse_and_extract_text(xml_path, output_path, exclude_texts)
--- debug/tttt.py
+++ debug/tttt.py
@@ -7,77 +7,39 @@ import tidevice
 from tidevice import Device
 import wda
 
-# driver = u2.connect_usb("28131FDH2000K1")
+
+def get_all_elements_texts(driver, scroll_pause=1):
+    """
+    获取当前页面的所有元素的text文本内容
+    :param scroll_pause:
+    :param max_scrolls:
+    :param driver: uiautomator2的Device对象
+    :return: 文本内容列表
+    """
+    xml_content_set = set()
+
+    # for _ in range(max_scrolls):
+        # 获取页面的 XML 结构
+    page_source = driver.source()
+    print(page_source)
+
+    # 将每次读取的xml内容添加到集合中
+    xml_content_set.add(page_source)
+
+    # 滑动屏幕
+    driver.swipe_up()
+    time.sleep(scroll_pause)  # 等待页面稳定
+
+        # 将每次读取的xml内容追加到文件中
+    with open("H:\\app-uiauto-test-dev\\debug\\destination4.xml", 'a', encoding='utf-8') as f:
+        for xml_content in xml_content_set:
+            f.write(xml_content + '\n')
+
+    return list(xml_content_set)
 
 
-# def read_device_file(device, device_path):
-#     """
-#     使用 uiautomator2 读取安卓设备中的文件内容
-#     :param device: uiautomator2 的 Device 对象
-#     :param device_path: 安卓设备中的文件路径
-#     :return: 文件内容
-#     """
-#     file_content = device.shell(f"cat {device_path}")
-#     return file_content
-#
-#
-# # 示例用法
-# if __name__ == "__main__":
-#     device_path = "/sdcard/destination.xml"  # 替换为安卓设备中的文件路径
-#     driver = u2.connect_usb("28131FDH2000K1")  # 连接设备
-#
-#     content = read_device_file(driver, device_path)
-#     print("文件内容如下：")
-#     print(content)
-
-# driver.push("destination.xml", "/sdcard/")
-
-
-# def read_device_file(device_path):
-#     """
-#     使用 tidevice 读取 iPhone 设备中的文件内容
-#     :param device_udid: iPhone 设备的 UDID
-#     :param device_path: iPhone 设备中的文件路径
-#     :return: 文件内容
-#     """
-# 连接设备
-# device = tidevice.Device(device_udid)
-
-# 使用 fsync cat 命令读取文件内容
-# file_content = device.fsync("cat", device_path)
-# os.system("tidevice fsync push H:\\app-uiauto-test-dev\\debug\\destination.xml /Downloads/destination.xml")
-# os.system("tidevice fsync tree /Downloads/")
-# file_content = os.system("tidevice fsync cat /Downloads/destination.xml")
-# content = file_content.decode('utf-8')
-
-# command = f"tidevice fsync cat {device_path}"
-#
-# # 执行命令并捕获输出
-# process = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-#                          encoding='utf-8', errors='ignore')
-# if process.returncode == 0:
-#     return process.stdout
-# else:
-#     print(f"读取文件失败: {process.stderr}")
-#     return None
-
-
-# 示例用法
-# if __name__ == "__main__":
-#     driver = wda.Client('http://localhost:8100')
-#     device_udid = "3db17f31334a5dbbe98c6d03b110f9ae0115e352"  # 替换为你的 iPhone 设备的 UDID
-#     device_path = "/Downloads/destination.xml"  # 替换为 iPhone 设备中的文件路径
-#
-#     content = read_device_file(device_path)
-#     if content:
-#         print("文件内容如下：")
-#         print(content)
-# 获取指定路径的绝对路径
-path = "./elements_texts.txt"
-abs_path = os.path.abspath(path)
-
-output_path = os.path.abspath("./elements_texts.txt")
-
-# 输出绝对路径
-print(abs_path)
-print(output_path)
+# 使用示例
+if __name__ == "__main__":
+    driver = wda.Client('http://localhost:8100')
+    texts = get_all_elements_texts(driver)
+    print(f"共获取到 {len(texts)} 段XML内容")
