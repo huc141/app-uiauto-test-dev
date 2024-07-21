2434e31c2ec0465df4af184099269e0fe2ad1c0f
huc141 <1349983371@qq.com>
2024-07-22 00:26:09 +0800

    修改read_yaml公共方法，使之可遍历config目录下的设备目录中的yaml文件

--- common_tools/read_yaml.py
+++ common_tools/read_yaml.py
@@ -9,20 +9,40 @@ class ReadYaml:
 
     def __init__(self):
         p = os.path.join(os.getcwd(), 'config')
-        for fname in os.listdir(p):
-            # 如果文件的扩展名不是 'yml' 或 'yaml'，则跳过这个文件，不执行后续的处理。这个逻辑用于过滤掉不是 YAML 文件的内容，只处理 'config' 目录下的 YAML 文件。
-            # fname.split('.')：这会将文件名（如 'example.yml'）按照点号分割成一个列表，即 ['example', 'yml']。
-            if fname.split('.')[-1] not in ['yml', 'yaml']:  # [-1]：这表示获取列表的最后一个元素，即文件扩展名。对于 'example.yml'，它会得到 'yml'。
-                continue
-            with open(file=os.path.join(p, fname), encoding='utf-8') as stream:  # 打开file文件，as stream：将打开的文件对象赋值给名为 stream 的变量
-                self.__raw_data[fname.split('.')[0]] = yaml.safe_load(stream)  # 获取文件名的前缀如case.yml中的case，作为__raw_data字典的键，值是从 YAML 文件加载得到的数据.
 
+        # 设备文件夹的根目录
+        config_root_dir = p
+
+        # 获取每个设备文件夹的路径
+        device_dirs = [os.path.join(config_root_dir, d) for d in os.listdir(config_root_dir) if
+                       os.path.isdir(os.path.join(config_root_dir, d))]
+
+        # 读取 config 文件夹下的所有 YAML 文件
+        for fname in os.listdir(p):
+            if fname.split('.')[-1] not in ['yml', 'yaml']:
+                continue
+            with open(file=os.path.join(p, fname),
+                      encoding='utf-8') as stream:  # 打开file文件，as stream：将打开的文件对象赋值给名为 stream 的变量
+                self.__raw_data[fname.split('.')[0]] = yaml.safe_load(
+                    stream)  # 获取文件名的前缀如case.yml中的case，作为__raw_data字典的键，值是从 YAML 文件加载得到的数据.
+
+        # 配置常规参数
         self.config_device_sn = self.get_data('config_device_sn', '')  # 获取手机序列号
         self.config_apk_name = self.get_data('config_apk_name', '')  # 获取安装包名称
         self.config_apk_local_path = self.get_data('apk_local_path')  # 获取安装包的本地地址
         self.wda_bundle_id = self.get_data('wda_bundle_id')  # 获取wda安装包名称
         self.uids_file_list = self.get_data('uids', source='uids')  # 获取uid列表
-        self.devices_main_remote_setting_config = self.get_data("devices", source="devices_main_remote_setting")  # 获取设备配置
+        self.devices_main_remote_setting_config = self.get_data("devices",
+                                                                source="devices_main_remote_setting")  # 获取设备配置
+
+        # 定义一个函数来加载指定设备文件夹中的 YAML 文件
+        def load_wifi_parse_xml(device_dir):
+            yaml_file_path = os.path.join(device_dir, 'wifi_parse_xml.yml')
+            with open(yaml_file_path, 'r', encoding='utf-8') as file:
+                return yaml.safe_load(file)
+
+        # 加载所有设备的 YAML 文件内容
+        self.wifi_configs = [load_wifi_parse_xml(device_dir) for device_dir in device_dirs]
 
     def get_data(self, key: str, default: str = '', source: str = 'phone') -> str:
         """Get specific config"""
