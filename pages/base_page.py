86eaf8288b683d1d4985c48e524dc822b2091cb2
luoyulong <luoyl@reolink.com.cn>
2024-08-22 20:38:52 +0800

    新增滚动并点击右侧按钮公共方法

--- pages/base_page.py
+++ pages/base_page.py
@@ -663,3 +663,77 @@ class BasePage:
                 logger.info(f"读取文件失败: {process.stderr}")
                 return None
 
+    def scroll_click_right_btn(self, text_to_find, el_type='text', max_attempts=1, scroll_pause=0.5):
+        """
+        在当前页面滑动查找并点击指定文本右侧的可点击元素/按钮。
+        :param el_type: 元素查找类型，支持 文本text(label).
+        :param text_to_find: 要查找的文本
+        :param max_attempts: 最大尝试次数
+        :param scroll_pause: 滚动后的暂停时间，秒
+        """
+        try:
+            def find_and_click_ios(xpath_exp):
+                element = self.driver.xpath(xpath_exp)
+                if element.exists:
+                    element.click()
+                    logger.info(f"尝试点击这个 '{text_to_find}' 元素右边的按钮")
+                    time.sleep(1)
+                    return True
+                return False
+
+            def click_button_android(text):
+                logger.info(f"尝试点击这个 '{text_to_find}' 元素右边的可点击按钮")
+                self.driver(text=text).right(clickable='true').click()
+                time.sleep(1)
+                return True
+
+            def click_button_ios(text):
+                # 这里的xpath表达式中的下标可能需要根据具体页面分析，先暂时这样写
+                if find_and_click_ios(
+                        f"//*[contains(@name, '{text}')]/following-sibling::*[3][@visible='true']"):
+                    return True
+                else:
+                    logger.info(f"没有找到目标元素{text}右边的按钮")
+                    return False
+
+            # 根据el_type初始化查找元素
+            if el_type == "text":
+                element = self.driver(text=text_to_find) if self.platform == "android" else self.driver(label=text_to_find)
+            else:
+                raise ValueError("你可能输入了不支持的元素查找类型")
+
+            attempt = 0
+
+            if self.platform == "android":
+                while attempt <= max_attempts:
+                    if element.exists:
+                        logger.info(f"【{text_to_find}】元素已找到")
+                        click_button_android(text_to_find)
+                        break
+                    else:
+                        # 滑动屏幕
+                        logger.info(f"尝试滑动查找 '{text_to_find}'... 第{attempt + 1}次")
+                        self.driver(scrollable=True).scroll(steps=150)
+
+                    attempt += 1
+
+            if self.platform == "ios":
+                # 判断元素是否可见，不可见则滑动至可见后点击
+                e_is_visible = self.driver(text=text_to_find)
+                while attempt <= max_attempts:
+                    if element.exists and e_is_visible.visible:
+                        logger.info(f"【{text_to_find}】元素存在且可见")
+                        click_button_ios(text_to_find)
+                        break
+                    else:
+                        # 获取可滑动的元素，此处根据className(Type)定位
+                        logger.info(f"尝试滑动查找 '{text_to_find}'... 第{attempt + 1}次")
+                        e = self.driver(className='XCUIElementTypeTable')
+                        e.scroll('down')
+                        time.sleep(scroll_pause)  # 等待页面稳定
+
+                    attempt += 1
+
+        except Exception as err:
+            logger.info(f"可能发生了错误: {err}")
+            return False
