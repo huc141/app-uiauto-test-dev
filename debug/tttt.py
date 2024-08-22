233491d04b96a8ca938a897a8044d01e76bbaddf
luoyulong <luoyl@reolink.com.cn>
2024-08-22 20:40:19 +0800

    调试

--- debug/tttt.py
+++ debug/tttt.py
@@ -10,135 +10,87 @@ import wda
 import importlib
 import os
 
-d = u2.connect_usb()
-wd = wda.Client('http://localhost:8100')
-wd(type='B').click()
+from common_tools.logger import logger
 
-# def get_all_texts(resource_id):
-#     all_texts = []
-#     previous_page_texts = []
-#     retry_count = 0
-#
-#     while True:
-#         # 获取当前页面所有元素的文本内容
-#         current_texts = []
-#         elements = d(resourceId=resource_id)
-#         for element in elements:
-#             text = element.get_text()
-#             if text not in all_texts:  # 避免重复添加
-#                 current_texts.append(text)
-#
-#         # 合并到所有文本列表
-#         all_texts.extend(current_texts)
-#
-#         # 检查是否没有新的内容
-#         if current_texts == previous_page_texts:
-#             retry_count += 1
-#             if retry_count >= 2:  # 连续两次滚动后没有新内容，认为获取完成
-#                 break
-#         else:
-#             retry_count = 0
-#
-#         previous_page_texts = current_texts
-#
-#         # 滚动页面
-#         d(scrollable=True).scroll.vert.forward()
-#         time.sleep(1)  # 等待滚动完成
-#
-#     return all_texts
-#
-#
-# # 获取所有元素的文本内容
-# resource_id = 'com.mcu.reolink:id/tv_remote_sub'  # 替换为实际的资源ID
-# all_texts = get_all_texts(resource_id)
-#
-# # 输出所有获取到的文本内容
-# print("获取到的所有文本内容：")
-# for text in all_texts:
-#     print(text)
+driver = u2.connect_usb()
+# driver = wda.Client('http://localhost:8100')
 
 
-# 获取页面所有功能名称
-start = time.perf_counter()
-
-
-def scroll_screen(driver, platform, max_scrolls=1):
+def scroll_click_right_btn(text_to_find, el_type='text', max_attempts=1, scroll_pause=0.5, platform='android'):
     """
-    滚动屏幕
-    :param driver: 驱动实例
-    :param platform: 平台类型 ("android" 或 "ios")
-    :param max_scrolls: 最大滚动次数，默认1次
-    :return:
+    在设备列表中滚动查找指定设备名称(支持单机、nvr、hub),并点击远程设置按钮。
+    :param platform:
+    :param el_type: 元素查找类型，支持 文本text(label).
+    :param text_to_find: 要查找的文本
+    :param max_attempts: 最大尝试次数
+    :param scroll_pause: 滚动后的暂停时间，秒
     """
-    platform = platform.lower()  # 将平台名称转换为小写，确保一致性
+    try:
+        def find_and_click_ios(xpath_exp):
+            element = driver.xpath(xpath_exp)
+            if element.exists:
+                element.click()
+                logger.info(f"尝试点击这个 '{text_to_find}' 元素右边的按钮")
+                time.sleep(1)
+                return True
+            return False
 
-    if platform == "android":
-        scroll_method = driver.swipe_ext
-        direction = "up"
-    elif platform == "ios":
-        scroll_method = driver.swipe_up
-    else:
-        raise ValueError(f"不支持当前平台: {platform}")
+        def click_button_android(text_to_find):
+            logger.info(f"尝试点击这个 '{text_to_find}' 元素右边的可点击按钮")
+            driver(text=text_to_find).right(clickable='true').click()
+            time.sleep(1)
+            return True
 
-    for _ in range(max_scrolls):
-        if platform == "android":
-            scroll_method(direction)
+        def click_button_ios(text):
+            # 这里的xpath表达式中的下标可能需要根据具体页面分析，先暂时这样写
+            if find_and_click_ios(
+                    f"//*[contains(@name, '{text}')]/following-sibling::*[3][@visible='true']"):
+                return True
+            else:
+                logger.info(f"没有找到目标元素{text}右边的按钮")
+                return False
+
+        # 根据el_type初始化查找元素
+        if el_type == "text":
+            element = driver(text=text_to_find) if platform == "android" else driver(label=text_to_find)
         else:
-            scroll_method()
-            time.sleep(0.5)  # iOS平台需要短暂等待UI更新
+            raise ValueError("你可能输入了不支持的元素查找类型")
+
+        attempt = 0
+
+        if platform == "android":
+            while attempt <= max_attempts:
+                if element.exists:
+                    logger.info(f"【{text_to_find}】元素已找到")
+                    click_button_android(text_to_find)
+                    break
+                else:
+                    # 滑动屏幕
+                    logger.info(f"尝试滑动查找 '{text_to_find}'... 第{attempt + 1}次")
+                    driver(scrollable=True).scroll(steps=150)
+
+                attempt += 1
+
+        if platform == "ios":
+            # 判断元素是否可见，不可见则滑动至可见后点击
+            e_is_visible = driver(text=text_to_find)
+            while attempt <= max_attempts:
+                if element.exists and e_is_visible.visible:
+                    logger.info(f"【{text_to_find}】元素存在且可见")
+                    click_button_ios(text_to_find)
+                    break
+                else:
+                    # 获取可滑动的元素，此处根据className(Type)定位
+                    logger.info(f"尝试滑动查找 '{text_to_find}'... 第{attempt + 1}次")
+                    e = driver(className='XCUIElementTypeTable')
+                    e.scroll('down')
+                    time.sleep(scroll_pause)  # 等待页面稳定
+
+                attempt += 1
+
+    except Exception as err:
+        logger.info(f"可能发生了错误: {err}")
+        return False
 
 
-def get_all_texts(selector_type, selector, max_scrolls=1):
-    """
-    滚动获取当前页面所有指定文本
-    :param selector_type: 支持安卓className，resource-id定位，iOS的className定位
-    :param selector: 对应的value值
-    :param max_scrolls: 最大滚动次数
-    :return:
-    """
-    my_set = set()
-
-    def get_elements_texts():
-        if selector_type == 'id':
-            elements = d(resourceId=selector)
-        elif selector_type == 'class':
-            elements = d(className=selector)
-        return {element.get_text() for element in elements}
-
-    # 获取当前页面所有元素的文本内容
-    for _ in range(max_scrolls):
-        # 获取当前页面所有元素的文本内容
-        new_texts = get_elements_texts()
-        my_set.update(new_texts)
-
-        scroll_screen(d, "android")
-        time.sleep(0.5)
-
-        # 检查滑动后页面是否有变化
-        new_texts = get_elements_texts()
-        if not new_texts - my_set:
-            break  # 如果滑动后没有新内容，退出循环
-
-        # 添加新获取的文本内容
-        my_set.update(new_texts)
-    print(my_set)
-    return list(my_set)
-
-
-resource_id = 'com.mcu.reolink:id/tv_remote_sub'  # 替换为实际的资源ID
-get_all_texts(selector_type='id', selector=resource_id)
-
-
-end = time.perf_counter()
-print("运行耗时", end - start)
-
-# 计算所获得的功能名称数量
-
-# 计算预期设备>预期页面的功能数量
-
-# 遍历预期名称，验证文本和数量是否相等
-
-
-# 示例用法
-# driver = d  # 替换为实际的驱动实例
-# scroll_screen(driver, platform="android", max_scrolls=1)
+scroll_click_right_btn(text_to_find='水印')
