fd0cf57365aa733595acdfdef300ee7b72fe0e3d
huc141 <1349983371@qq.com>
2024-08-26 00:39:27 +0800

    新增用例

--- pages/rn_device_setting_page/remote_display.py
+++ pages/rn_device_setting_page/remote_display.py
@@ -15,18 +15,29 @@ class RemoteDisplay(BasePage):
         elif self.platform == 'ios':
             pass
 
-    def access_in_stream(self, text_list, option_text='码流'):
+    def access_in_stream(self, option_text='码流'):
         """
         进入并测试码流页面
-        :param text_list: 文本
         :param option_text: 菜单功能项，该方法默认进入【码流】
         :return:
         """
         # 进入码流页面
         self.scroll_and_click_by_text(text_to_find=option_text)
 
-        # 检查码流页面文案
-        page_fun_list = RemoteSetting().scroll_check_funcs(text_list)
+    def access_in_clear(self, option_text='清晰'):
+        """
+        进入码流>清晰页面
+        :param option_text: 菜单功能项，该方法默认进入【码流>清晰】
+        :return:
+        """
+        # 进入码流>清晰页面
+        self.scroll_and_click_by_text(text_to_find=option_text)
 
-        # 断言
-        assert page_fun_list is True
+    def click_resolution(self, option_text='分辨率'):
+        """
+        :param option_text: 菜单功能项，该方法默认点击【分辨率】
+        :return:
+        """
+        self.scroll_and_click_by_text(text_to_find=option_text)
+
+
