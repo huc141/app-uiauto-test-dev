63e50c70b301cedc3b3c2b02405ba1ed115b2631
luoyulong <luoyl@reolink.com.cn>
2024-09-25 21:13:09 +0800

    调试

--- test_case/remote_setting/test_remote_detection_alarm.py
+++ test_case/remote_setting/test_remote_detection_alarm.py
@@ -7,3 +7,11 @@ from pages.base_page import BasePage
 from pages.rn_device_setting_page.remote_setting import RemoteSetting
 
 devices_config = read_yaml.load_device_config(yaml_file_name='light.yaml')  # 读取参数化文件
+
+
+@allure.feature("远程配置>侦测报警")
+class TestRemoteDetectionAlarm:
+    @pytest.mark.parametrize("device_config", devices_config)
+    @allure.feature("测非侦测区域")
+    def test_remote_non_detection_area(self, device_config):
+        pass
