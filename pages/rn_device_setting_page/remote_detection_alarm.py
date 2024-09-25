60686d70a8af3f5ab79f084e9913c7f333ffed9c
huc141 <1349983371@qq.com>
2024-09-26 00:06:09 +0800

    新增非侦测区域公共方法

--- pages/rn_device_setting_page/remote_detection_alarm.py
+++ pages/rn_device_setting_page/remote_detection_alarm.py
@@ -10,9 +10,50 @@ class RemoteDetectionAlarm(BasePage):
     def __init__(self):
         super().__init__()
         if self.platform == 'android':
-            self.time_selector_hour = ''
-            self.time_selector_min = ''
+            self.non_detection_area = '//*[@resource-id="com.mcu.reolink:id/ziv"]'  # 非侦测区域屏幕
+            self.non_detection_area_fullscreen_button = '//*[@resource-id="com.mcu.reolink:id/player_fullscreen_button"]'  # 非侦测区域横屏按钮
+            self.landscape_bar_portrait_button = '//*[@resource-id="com.mcu.reolink:id/landscape_bar_portrait"]'  # 非侦测区域横屏左上角按钮恢复竖屏按钮
 
         elif self.platform == 'ios':
-            self.time_selector_hour = ''
-            self.time_selector_min = ''
+            self.non_detection_area = ''
+            self.non_detection_area_fullscreen_button = ''
+
+    def click_non_detection_area(self):
+        """
+        点击进入非侦测区域
+        :return:
+        """
+        self.scroll_and_click_by_text(text_to_find='非侦测区域')
+
+    def draw_portrait_non_detection_area(self, mode='xpath', draw_area='左上'):
+        """
+        竖屏：绘制非侦测区域，这里的策略是：首先点击全部绘制，然后点击全部擦除，然后点击绘制并在屏幕上绘制一部分内容，最后再点击擦除并对刚刚绘制的区域进行擦除操作
+        :return:
+        """
+        self.scroll_and_click_by_text(text_to_find='全部绘制')
+
+        self.scroll_and_click_by_text(text_to_find='全部擦除')
+
+        self.scroll_and_click_by_text(text_to_find='绘制')
+        self.get_coordinates_and_draw(mode=mode, id_or_xpath=self.non_detection_area, draw_area=draw_area)
+        self.scroll_and_click_by_text(text_to_find='擦除')
+        self.get_coordinates_and_draw(mode=mode, id_or_xpath=self.non_detection_area, draw_area=draw_area)
+
+    def draw_landscape_non_detection_area(self, mode='xpath', draw_area='左上'):
+        """
+        横屏：绘制非侦测区域，这里的策略是：首先点击全部绘制，然后点击全部擦除，然后点击绘制并在屏幕上绘制一部分内容，最后再点击擦除并对刚刚绘制的区域进行擦除操作
+        :return:
+        """
+        # 点击横屏按钮
+        self.scroll_and_click_by_text(text_to_find=self.non_detection_area_fullscreen_button, el_type='xpath')
+
+        self.scroll_and_click_by_text(text_to_find='全部绘制')
+        self.scroll_and_click_by_text(text_to_find='全部擦除')
+
+        self.scroll_and_click_by_text(text_to_find='绘制')
+        self.get_coordinates_and_draw(mode=mode, id_or_xpath=self.non_detection_area, draw_area=draw_area)
+        self.scroll_and_click_by_text(text_to_find='擦除')
+        self.get_coordinates_and_draw(mode=mode, id_or_xpath=self.non_detection_area, draw_area=draw_area)
+
+        # 点击左上角按钮恢复竖屏
+        self.scroll_and_click_by_text(text_to_find=self.landscape_bar_portrait_button, el_type='xpath')
