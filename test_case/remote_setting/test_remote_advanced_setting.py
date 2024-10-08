45c4b405eb7bedbe7a67e80bf1c048febfa20363
huc141 <1349983371@qq.com>
2024-10-08 23:15:29 +0800

    新增

--- test_case/remote_setting/test_remote_advanced_setting.py
+++ test_case/remote_setting/test_remote_advanced_setting.py
@@ -24,7 +24,7 @@ class TestRemoteAdvancedSetting:
         # 启动app，并开启录屏
         driver.start_app(True)
 
-        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击菜单项@allure.feature进入侦测报警
+        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击菜单项@allure.feature
         RemoteSetting().access_in_advanced(device_list_name=device_config['device_list_name'])
 
         # 验证主页文案
@@ -45,12 +45,47 @@ class TestRemoteAdvancedSetting:
         # 启动app，并开启录屏
         driver.start_app(True)
 
-        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击菜单项@allure.feature进入侦测报警
+        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击菜单项@allure.feature
         RemoteSetting().access_in_advanced(device_list_name=device_config['device_list_name'])
 
-        # 点击并进入用户管理主页，验证非法登录锁定
+        # 点击并进入用户管理主页
+        RemoteAdvancedSetting().click_user_management()
+        # 验证非法登录锁定
+        texts_res = RemoteAdvancedSetting().click_illegal_login_lockout(texts=remote_items['user_management']['illegal_login_lockout']['text'])
 
+        # 验证管理员栏的文案
+        manager_texts_res = RemoteAdvancedSetting().check_manager_text(texts=remote_items['user_management']['manager']['text'])
 
+        # 验证用户栏文案
+        user_texts_res = RemoteAdvancedSetting().check_user_text(texts=remote_items['user_management']['user']['text'])
+
+        # 断言
+        assert texts_res is True
+        assert manager_texts_res is True
+        assert user_texts_res is True
+
+    @pytest.mark.parametrize("device_config", devices_config)
+    @allure.feature("日期和时间")
+    @allure.story("需人工核查日志和录屏")
+    def test_user_management(self, device_config):
+        # 检查键是否存在，存在则执行当前用例，否则跳过
+        remote_items = device_config['ipc']['advanced_setting']['items']['advanced_setting']
+        BasePage().check_key_in_yaml(remote_items, 'user_management')
+
+        # 启动app，并开启录屏
+        driver.start_app(True)
+
+        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击菜单项@allure.feature
+        RemoteSetting().access_in_advanced(device_list_name=device_config['device_list_name'])
+
+        # 点击并进入用户管理主页
+        RemoteAdvancedSetting().click_user_management()
+
+        # 点击进入日期和时间，验证文案
+        date_time_texts_res = RemoteAdvancedSetting().click_and_check_date_time_text(texts=remote_items['date_and_time']['text'])
+
+        # 断言
+        assert date_time_texts_res is True
 
 
 
