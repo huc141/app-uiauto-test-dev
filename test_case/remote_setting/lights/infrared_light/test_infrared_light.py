3e55d1ecc359742cfdd355e3ff5d22ea1336ada3
luoyulong <luoyl@reolink.com.cn>
2024-11-05 17:29:36 +0800

    将红外灯用例拆出来

---
+++ test_case/remote_setting/lights/infrared_light/test_infrared_light.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import pytest
+import allure
+from common_tools.app_driver import driver
+from common_tools.read_yaml import read_yaml
+from pages.base_page import BasePage
+from pages.rn_device_setting_page.remote_setting import RemoteSetting
+from pages.rn_device_setting_page.remote_light import RemoteLight
+
+devices_config = read_yaml.load_device_config(yaml_file_name='light.yaml')  # 读取参数化文件
+
+
+@allure.epic("远程配置>常规设置>灯")
+class TestRemoteLight:
+
+    @pytest.mark.parametrize("device_config", devices_config)
+    @allure.feature("灯>红外灯")
+    @allure.story("需人工核查日志和录屏")
+    @pytest.mark.skip
+    def test_remote_infrared_light(self, device_config):
+        # 检查键是否存在，存在则执行当前用例，否则跳过
+        remote_items = device_config['ipc']['light']['items']['light']
+        BasePage().check_key_in_yaml(remote_items, 'infrared_light')
+
+        # 启动app，并开启录屏
+        driver.start_app(True)
+
+        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击‘灯’菜单项进入灯主页
+        RemoteSetting().access_in_light(device_list_name=device_config['device_list_name'])
+
+        # 点击并测试红外灯和配置
+        lights_num = RemoteLight().verify_lights_list_length(texts=remote_items['text'])  # 判断灯数量
+        infrared_main_text_res = RemoteLight().click_and_test_infrared_light(lights_num=lights_num,
+                                                                             infrared_light_texts=
+                                                                             remote_items['infrared_light'][
+                                                                                 'subpage']['text'],
+                                                                             options_text=
+                                                                             remote_items['infrared_light'][
+                                                                                 'subpage']['options_text'])
+
+        assert infrared_main_text_res is True
+
+
