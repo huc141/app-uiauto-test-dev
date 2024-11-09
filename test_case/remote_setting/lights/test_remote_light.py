330aec7d66ffbbee82024ae7dea996283dab12bd
huc141 <1349983371@qq.com>
2024-11-09 13:51:50 +0800

    取消注释

--- test_case/remote_setting/lights/test_remote_light.py
+++ test_case/remote_setting/lights/test_remote_light.py
@@ -21,10 +21,10 @@ class TestRemoteLight:
         BasePage().check_key_in_yaml(remote_items, 'light')
 
         # 启动app，并开启录屏
-        # driver.start_app(True)
+        driver.start_app(True)
 
         # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击‘灯’菜单项进入灯主页
-        # RemoteSetting().access_in_light(device_list_name=device_config['device_list_name'])
+        RemoteSetting().access_in_light(device_list_name=device_config['device_list_name'])
 
         # 验证灯主页文案
         lights_num = RemoteLight().verify_lights_list_length(texts=remote_items['light']['text'])  # 判断灯数量
