fd366777d103f22efc6da00559083fee248e8703
huc141 <1349983371@qq.com>
2024-11-19 00:22:13 +0800

    新增test_remote_pre_record_plan用例，待调试

--- test_case/remote_setting/pre_recording/precording_plan/test_precord_plan.py
+++ test_case/remote_setting/pre_recording/precording_plan/test_precord_plan.py
@@ -16,7 +16,7 @@ class TestRemotePreRecording:
     @pytest.mark.parametrize("device_config", devices_config)
     @allure.feature("预录模式>计划")
     @allure.story("需人工核查日志和录屏")
-    def test_remote_pre_record_fps(self, device_config):
+    def test_remote_pre_record_plan(self, device_config):
         # 检查键是否存在，存在则执行当前用例，否则跳过
         remote_items = device_config['ipc']['pre_recording']['items']['pre_recording']
         BasePage().check_key_in_yaml(remote_items, 'add_new_plan')
@@ -27,9 +27,13 @@ class TestRemotePreRecording:
         # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击‘预录模式’菜单项进入
         RemoteSetting().access_in_remote_pre_recording(device_list_name=device_config['device_list_name'])
 
-        # ======== 验证计划=========
-
-        illegal_funcs_res = BasePage().detect_illegal_functions(legal_funcs_ids=remote_items['FPS']['text'])
+        # ========= 验证计划 =========
+        result = RemotePreRecord().verify_precording_plan(plan_texts=remote_items['pre_record_plan']['text'],
+                                                          new_plan_texts=remote_items['add_new_plan']['text'])
 
         # 断言
-        assert illegal_funcs_res is True
+        assert result['plan_texts_res'] is True
+        assert result['plan_illegal_funcs_res'] is True
+        assert result['new_plan_texts_res'] is True
+        assert result['new_plan_illegal_funcs_res'] is True
+        assert result['custom_plan_res'] is True
