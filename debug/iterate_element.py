c2d8ee00efc9aefec1097a73cd2b70d4c1dfc99c
luoyulong <luoyl@reolink.com.cn>
2024-12-09 18:28:09 +0800

    调试

--- debug/iterate_element.py
+++ debug/iterate_element.py
@@ -63,17 +63,21 @@ def find_element_by_xpath_recursively(d, xpath_prefix, target_id=None, target_te
 d = u2.connect()  # 请根据实际情况填写设备IP或序列号
 
 # 定义起始元素的xpath路径
-start_xpath = '//android.widget.TextView[@text="B83S"]'
+start_xpath = '//*[@resource-id="Brightness"]'
 
 # 调用方法查找目标元素
 target_element = find_element_by_xpath_recursively(d,
                                                    start_xpath,
                                                    target_id="RNE__Slider_Thumb")
 
-# 检查查找结果
-if target_element:
-    print("找到目标元素:", target_element.info)
-    target_element.swipe('right')
-else:
-    print("未找到目标元素")
 
+def operate_on_element(element):
+    # 检查查找结果
+    if element:
+        print("找到目标元素:", element.info)
+        element.swipe('right')
+    else:
+        print("未找到目标元素")
+
+
+operate_on_element(target_element)
