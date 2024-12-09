5e0e317709c5e5d9c1be1210181b0dd55c57562f
luoyulong <luoyl@reolink.com.cn>
2024-12-09 18:30:40 +0800

    白天和黑夜模块的用例需要拆分，待完成

--- test_case/remote_setting/display/anti_flicker_day_and_night_bright/test_anti_flicker_day_and_night_bright.py
+++ test_case/remote_setting/display/anti_flicker_day_and_night_bright/test_anti_flicker_day_and_night_bright.py
@@ -7,35 +7,15 @@ from pages.base_page import BasePage
 from pages.rn_device_setting_page.remote_setting import RemoteSetting
 from pages.rn_device_setting_page.remote_display import RemoteDisplay
 
-devices_config = read_yaml.load_device_config(yaml_file_name='display.yaml')  # 读取参数化文件
+devices_config = read_yaml.load_device_config(device_dir='apower/AReolink_TrackMix_WiFi', yaml_file_name='display.yaml')  # 读取参数化文件
 
 
 # TODO: 需要适配为RN
-@allure.epic("远程配置>常规设置>显示")
+@allure.epic("远程配置>常规设置>显示>白天和黑夜")
 class TestRemoteDisplay:
-    @pytest.mark.parametrize("device_config", devices_config)
-    @allure.feature("显示>抗闪烁")
-    def test_remote_anti_flicker(self, device_config):
-        # 检查键是否存在，存在则执行当前用例，否则跳过
-        remote_items = device_config['ipc']['display']['items']['display']
-        BasePage().check_key_in_yaml(remote_items, 'anti_flicker')
-
-        # 启动app，并开启录屏
-        driver.start_app(True)
-
-        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击‘显示’菜单项进入显示页
-        RemoteSetting().access_in_display(device_list_name=device_config['device_list_name'])
-
-        # 点击抗闪烁，验证popup文本
-        RemoteDisplay().click_anti_flicker()
-        RemoteSetting().scroll_check_funcs2(remote_items['anti_flicker']['text'])
-        RemoteSetting().scroll_check_funcs2(remote_items['anti_flicker']['options'], selector='ReoTitle')
-        # 遍历popup操作项
-        BasePage().iterate_and_click_popup_text(
-            option_text_list=remote_items['anti_flicker']['options'], menu_text='抗闪烁')
 
     @pytest.mark.parametrize("device_config", devices_config)
-    @allure.feature("显示>白天和黑夜")
+    @allure.feature("白天和黑夜")
     def test_remote_day_and_night(self, device_config):
         # 检查键是否存在，存在则执行当前用例，否则跳过
         remote_items = device_config['ipc']['display']['items']['display']
@@ -54,19 +34,3 @@ class TestRemoteDisplay:
         # 遍历popup操作项
         BasePage().iterate_and_click_popup_text(
             option_text_list=remote_items['anti_flicker']['options'], menu_text='白天和黑夜')
-
-    @pytest.mark.parametrize("device_config", devices_config)
-    @allure.feature("显示>亮度")
-    def test_remote_brightness(self, device_config):
-        # 检查键是否存在，存在则执行当前用例，否则跳过
-        remote_items = device_config['ipc']['display']['items']['display']
-        BasePage().check_key_in_yaml(remote_items, 'brightness')
-
-        # 启动app，并开启录屏
-        driver.start_app(True)
-
-        # 设备列表中滚动查找到单机、nvr、hub并进入远程配置，在远程设置主页点击‘显示’菜单项进入显示页
-        RemoteSetting().access_in_display(device_list_name=device_config['device_list_name'])
-
-        # 拖动亮度条
-        RemoteDisplay().drag_slider_brightness(slider_mode='')
