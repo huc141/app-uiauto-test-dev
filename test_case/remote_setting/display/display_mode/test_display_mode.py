4fcc283bccd386245f285164a6f789720dfc01f2
huc141 <1349983371@qq.com>
2024-12-12 00:15:24 +0800

    新增test_display_mode用例，待完成

--- test_case/remote_setting/display/display_mode/test_display_mode.py
+++ test_case/remote_setting/display/display_mode/test_display_mode.py
@@ -10,6 +10,7 @@ from pages.rn_device_setting_page.remote_display import RemoteDisplay
 devices_config = read_yaml.load_device_config(device_dir='apower/AReolink_TrackMix_WiFi',
                                               yaml_file_name='display.yaml')  # 读取参数化文件
 
+
 @allure.feature("远程配置>常规设置>显示")
 class TestRemoteDisplay:
     @pytest.mark.parametrize("device_config", devices_config)
@@ -17,4 +18,5 @@ class TestRemoteDisplay:
     @allure.story("需人工核查日志和录屏")
     @allure.title("测试进入显示模式页面，分别点击不同的显示模式类型")
     def test_display_mode(self, device_config):
+        # TODO:
         pass
